32cfef3c1ea8abf586d9c820aa56cd48a541276d
VaL <143123781+VaL2111@users.noreply.github.com>
2024-01-15 11:41:28 +0200

    Update sample.js
    add 'use strict';

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,5 +1,7 @@
 // Get one random element from an array
 
+'use strict';
+
 const getRandomElement = (arr) => {
   const randomIndex = Math.floor(Math.random() * arr.length);
   return arr[randomIndex];
