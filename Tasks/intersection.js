f14c1ebea678c57b9d1882086c0742c2f5700519
VaL <143123781+VaL2111@users.noreply.github.com>
2024-01-15 10:14:48 +0200

    Update intersection.js
    fixed the bug

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -4,8 +4,8 @@ const intersection = function intersection(object_1, object_2) {
   const keysObject1 = Object.keys(object_1);
   
   for (const attributeName of keysObject1) {
-    const valueObject1 = object_1(attributeName);
-    const valueObject2 = object_2(attributeName);
+    const valueObject1 = object_1[attributeName];
+    const valueObject2 = object_2[attributeName];
 
     if (valueObject1 === valueObject2) {
       object_2[attributeName] = valueObject1;
