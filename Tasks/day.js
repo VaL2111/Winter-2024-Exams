e6084e0627e433b808ddd2c7b05aa8a1e1273e40
VaL <143123781+VaL2111@users.noreply.github.com>
2024-01-15 09:38:19 +0200

    Update day.js
    rename and rotation

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,7 +1,5 @@
 // Get day number
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
 _parse_day_ = (s) => {
   let i;
   for (i = 0; i < D.length; i++) {
@@ -11,5 +9,7 @@ _parse_day_ = (s) => {
   }
   return -1;
 };
+
+const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 module.exports = _parse_day_;
