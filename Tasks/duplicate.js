0fcdd13280db32e31b4af0630bf51b1dd056cdeb
VaL <143123781+VaL2111@users.noreply.github.com>
2024-01-15 09:45:08 +0200

    Update duplicate.js
    small changes

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,8 +1,9 @@
 // Return an array without duplicates
 
-duplicate = (value, N) => {
-  if (N <= 0) return [];
-  else {
+const duplicate = (value, N) => {
+  if (N <= 0){
+     return [];
+  } else {
     res = [];
     for (let i = 0; i < N; i++) {
       res[i] = value;
