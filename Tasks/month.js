2d6c842b94711b2ce36f35f028d968e0565c8fcd
VaL <143123781+VaL2111@users.noreply.github.com>
2024-01-15 10:27:34 +0200

    Update month.js
    fixed bug

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,9 +1,8 @@
 // Get month number
 
 const getMonthNumber = (s) => {
-  const input = s.toLowerCase();
   for (let i = 0; i < months.length; i++) {
-    if (input.startsWith(Months[i])) {
+    if (s.toLowerCase().startsWith(Months[i])) {
       return i + 1;
     }
   }
